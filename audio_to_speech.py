67b553b618745a55331d130ba523570ac720d217
Jashanjot <jashanjotpanjrath@gmail.com>
2025-03-01 22:06:49 -0500

    Added env files

--- audio_to_speech.py
+++ audio_to_speech.py
@@ -1,6 +1,10 @@
 import assemblyai as aai
+from dotenv import load_dotenv
+import os
 
-aai.settings.api_key = ""
+load_dotenv()
+
+aai.settings.api_key = os.getenv("ASSEMBLYAI_API_KEY")
 transcriber = aai.Transcriber()
 
 transcript = transcriber.transcribe("./audio.mp4")
